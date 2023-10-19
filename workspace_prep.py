4389dd1943c4dd5a10fb045a25603c54a835b548
MahmoudYidi <dopemubiks@gmail.com>
2023-10-19 12:54:00 +0100

    Setup

---
+++ workspace_prep.py
@@ -0,0 +1,11 @@
+### Import OpenCV & numpy #####
+
+##### Workspace prep
+# Python Extension
+#Pylance Extension
+#Path Intellisense
+#Visual Studio IntelliCode
+
+import cv2
+
+print(cv2.__version__)
